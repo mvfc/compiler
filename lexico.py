3253fa3c3decbfbbc1a5b1cda3a8ef7068c51044
Matheus Vitório <mvfc@users.noreply.github.com>
2017-10-28 21:00:18 -0200

    Update lexico.py

--- lexico.py
+++ lexico.py
@@ -1,196 +1,38 @@
 from collections import namedtuple
 
-			####D E L  >= <= +  -  " " . <- >  <  =  *  \  (  )  { } ; \. eof
-transicao = [[1,99,14,17,17,10,10,6,99,99,13,17,17,17,10,10,11,99,8,99,16,99,15 ],
-	   		[1,4,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,2],
-	   		[3,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99],
-			[3,4,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99],
-			[99,99,99,99,99,18,18,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99],
-			[5,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99],
-			[99,99,99,99,99,99,99,99,7,6,99,99,99,99,99,99,99,99,99,99,99,99,99],
-			[99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99],
-			[99,99,99,99,99,99,99,99,99,99,8,99,99,99,99,99,99,99,99,9,99,99,99],
-			[99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99],
-			[99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99],
-			[99,99,99,99,99,99,99,99,99,19,99,99,99,99,99,99,99,12,99,99,99,99,99],
-			[99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99],
-			[99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99],
-			[14,99,14,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99],
-			[99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99],
-			[99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99],
-			[99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99],
-			[15,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99],
-			[99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,12,99,99,99,99,99]]
-
-global posicao
-
-
-def consulta(x):
-	global posicao
-	j = 0 #armazenando estado
-	i = 0 #string[0]
-	if(x[i].isdigit() == True):		
-		while(i+1 < len(x) and x[i].isdigit() == True):
-			i = i + 1
-			j = transicao[j][0]
-			
-		if(x[i] == "E"):
-			j = transicao[j][1]
-			i = i + 1
-		if(x[i] == "\."):
-			j = transicao[j][21]
-			i = i + 1
-		
-		if(j == 1):
-			posicao = i
-			return 1 #token e um digito
-		
-		if(j == 2 and x[i].isdigit == True):
-			while(i+1 < len(x) and x[i].isdigit() == True):
-				j = transicao[j][0]
-				i = i + 1
-			if(x[i] == "E"):
-				j = transicao[j][1]
-				i = i + 1
-			if(j == 3):
-				posicao = i
-				return 1 #token e um numerico
-		if(j == 4):
-			if(x[i] == "+" | x[i] == "-"):
-				j = transicao[j][5]
-				i = i + 1
-			if(x[i].isdigit() == True):
-				j = transicao[j][0]
-			while(i+1 < len(x) and x[i].isdigit() == True):
-				j = transicao[j][0]
-				i = i + 1
-#<-
-	if(x[0] == "<" and x[1] == "-"):
-		posicao = 1
-		return 9
-	#>=			
-	if(x[0] == ">" and x[1] == "="):
-		posicao = 1
-		j = transicao[j][3]
-		return 2
-	
-	#<=
-	if(x[0] == "<" and x[1] == "="):
-		posicao = 1
-		j = transicao[j][4]
-		return 2
-	
-	#<>
-	if(x[0] == "<" and x[1] == ">"):
-		posicao = 1
-		j = transicao[j][4]
-		return 2
-	
-	#> ou < ou = 
-	if(x[0] == ">" or x[0] == "<" or x[0] == "="):
-		posicao = 0
-		j = transicao[j][4]
-		return 2
-	
-	###" ... "
-	if(x[0] == "\""):
-		j = transicao[j][7]
-	   	while(x[i] != "\""):
-		      i = i + 1 
-		      if(x[i] == "\""):
-			 	j = transicao[j][8]
-		posicao = i
-		return 4
-	
-	#{...}
-	if(x[0] == "{"):
-		j = transicao [j][18]
-		while(x[i] != "}"):
-		      i = i + 1
-		      if(x[i] == "}"):
-		      	j = transicao[j][19]
-		posicao = i
-		return 5
-		      
- 	#(..)
-	if(x[0] == "("):
-		j = transicao[j][11]
-		while(x[i] != ")"):
-		      i = i + 1	
-		      if(x[i] == ")"):
-		      	j = transicao[j][12]
-		posicao = i
-		return 6
-#;
-	if(x[0] == ";"):
-		j = transicao[j][20]
-		posicao = i
-		return 7
-		
-	#operandos
-	if(x[0] == "+" or x[0] == "-" or x[0] == "/" or x[0] == "*"):
-		j = transicao[j][10]
-		posicao = 0
-		return 8
-		      
-
-#id
-	if(x[0].isalpha() == True):
-		if(len(x) > 1):
-			while(i+1 < len(x) and x[i].isalpha() == True or x[i].isdigit() == True or x[i] == "_"):
-				i = i + 1
-		if(x[i].isalpha() == True or x[i].isdigit() == True or x[i] == "_"):
-			posicao = i
-			return 10
-#eof
-	if(x[0] == ""):
-		posicao = 0
-		return 11
-
-			 
-
 node = namedtuple("node", "Token Lexema Tipo")
 
 c = node("inicio", "inicio", "rsv")
-d = node("varinicio","varinicio","rsv")
+d = node("varinicio", "varinicio", "rsv")
 e = node("varfim", "varfim", "rsv")
 f = node("escreva", "escreva", "rsv")
 g = node("leia", "leia", "rsv")
 h = node("se", "se", "rsv")
-i = node("entao","entao", "rsv")
-j = node("fimse","fimse", "rsv")
-k = node("fim","fim", "rsv")
-l = node("Inteiro","Inteiro", "rsv")
-m = node("entao","entao", "rsv")
-n = node("literal","literal", "rsv")
-o = node("real","real", "rsv")
+i = node("entao", "entao", "rsv")
+j = node("fimse", "fimse", "rsv")
+k = node("fim", "fim", "rsv")
+l = node("Inteiro", "Inteiro", "rsv")
+m = node("entao", "entao", "rsv")
+n = node("literal", "literal", "rsv")
+o = node("real", "real", "rsv")
 
 tabelaSimbolo = [c, d, e, f, g, h, i, j, k, l, m, n, o]
 
-def criaNode(token,lexema,tipo):
-	new = node(token,lexema,tipo)
-	if(nodeExists(new) == 0):
-		tabelaSimbolo.append(new)
-
-def nodeExists(node):
-	i = 0
-	while(len(tabelaSimbolo) > i):
-		if(node == tabelaSimbolo[i]):
-			return 1
-		i = i + 1
-	return 0
-
 arquivo = open("texto.alg")
-linha = arquivo.readline()
-nrlinha = 1
 
-while(linha != ""):
-	global posicao
-	posicao = 0
-	print linha
-	consulta(linha)
-	posicao = posicao + 1
-	linha = linha[posicao:]
-	print posicao
+def lexico(linha):
+    if(linha[0].isalpha() == True):
 
-arquivo.close()
+    if(linha[0].isdigit() == True):
+
+    if(linha[0] == ""):
+
+    if(linha[0] == "("):
+
+    if(linha[0] == "\""):
+
+    if(linha[0] == ")"):
+
+    if(linha[0] == "+" or linha[0] == "-" or linha[0] == "/" or linha[0] == "*"):
+
+    if(linha[0] == )
