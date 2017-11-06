760f50e95d6ec4ee87e51a43defe94c923065d32
ommathias <mathias.ufg@gmail.com>
2017-11-05 23:11:21 -0200

    Update sintatico.py

--- sintatico.py
+++ sintatico.py
@@ -1,4 +1,4 @@
 import lexico
 
 #############inicio/varinicio/varfim/ id /; / int /real/literal/num /rcb  /opm  /se  /(  /)  /entao/opr /fimse/ $ / P / V / LV /  D/ TIPO/A  /ES  /ARG /CMD /LD  /OPRD /COND/CABECALHO/EXP_R/CORPO
-tabela_slr = [ ]
+tabela_slr = [2    ,      99,     99, 99, 99,  99,  99,     99,  99,   99,   99,  99,  99, 99,  99,  99,    99,99, 99, 99,  99,  99,   99,99,  99,  99,  99,  99,   99,  99,        99,   99,   99]
