c4b314f0fb83f0897c2391896e7c2add7aabd933
ommathias <mathias.ufg@gmail.com>
2017-11-05 23:35:19 -0200

    Update sintatico.py

--- sintatico.py
+++ sintatico.py
@@ -1,4 +1,6 @@
 import lexico
 
-#############inicio/varinicio/varfim/ id /; / int /real/literal/num /rcb  /opm  /se  /(  /)  /entao/opr /fimse/ $ / P / V / LV /  D/ TIPO/A  /ES  /ARG /CMD /LD  /OPRD /COND/CABECALHO/EXP_R/CORPO
-tabela_slr = [2    ,      99,     99, 99, 99,  99,  99,     99,  99,   99,   99,  99,  99, 99,  99,  99,    99,99, 99, 99,  99,  99,   99,99,  99,  99,  99,  99,   99,  99,        99,   99,   99]
+#############inicio | varinicio | varfim | id| ; | int | real | leia | escreva | literal | num | rcb | opm | se | ( | ) | entao | opr | fimse | fim | $ | P | V | LV | D | TIPO | A | ES | ARG | CMD | LD | OPRD | COND | CABECALHO| EXP_RCORPO
+tabela_slr = [    2,      99,     99,      99, 99,  99,  99,     99,     99,     99,        99,  99,   99,   99,  99, 99,   99,   99,    99,     99,  99, 1,  99, 99,  99,  99,  99,  99,   99,  99,   99,   99,   99,    99]
+             [   99,      99,     99,      99, 99,  99,  99,     99,     99,     99,        99,  99,   99,   99,  99, 99,   99,   99,    99,     99,  00, 99, 99, 99,  99,  99,  99,  99,   99,  99,   99,   99,   99,    99]
+             [   99,      99,     99, 12, 99,  99,  99,     99,  99,   99,   99,
