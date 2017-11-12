e02b6a236ef3bc06293b0ab2bff9aec3d9b088aa
Matheus Vitório <mvfc@users.noreply.github.com>
2017-11-12 20:03:18 -0200

    Update analisadorsintatico.py

--- analisadorsintatico.py
+++ analisadorsintatico.py
@@ -1,6 +1,6 @@
 import lexico
 
-tabela_slr = [   2,       99,      99,     99,   99,  99,  99,     99,     99,     99,        99,  99,   99,   99,  99, 99,   99,   99,    99,     99,   99,   1,  99, 99,  99,  99,  99,  99,   99,  99,   99,   99,   99,         99,    99,    99],
+tabela_slr = [[   2,       99,      99,     99,   99,  99,  99,     99,     99,     99,        99,  99,   99,   99,  99, 99,   99,   99,    99,     99,   99,   1,  99, 99,  99,  99,  99,  99,   99,  99,   99,   99,   99,         99,    99,    99],
 [   99,      99,      99,     99,   99,  99,  99,     99,     99,     99,        99,  99,   99,   99,  99, 99,   99,   99,    99,     99,   00,   99, 99, 99,  99,  99,  99,  99,   99,  99,   99,   99,   99,         99,    99,    99],
 [   99,       4,      99,     99,   99,  99,  99,     99,     99,     99,        99,  99,   99,   99,  99, 99,   99,   99,    99,     99,   99,   99,  3, 99,  99,  99,  99,  99,   99,  99,   99,   99,   99,         99,    99,    99],
 [   99,      99,      99,     12,   99,  99,  99,     10,     11,     99,        99,  99,   99,   13,  99, 99,   99,   99,    99,      9,   99,   99, 99, 99,  99,  99,   5,   7,   99,   8,   99,   99,    6,         14,    99,    99],
@@ -58,14 +58,15 @@ tabela_slr = [   2,       99,      99,     99,   99,  99,  99,     99,     99,  
 [   99,      99,      99,      99, 99,   99,  99,     99,     99,     99,        99,  99,   99,   99,  99, 99,   99,   99,    30,     99,   99,   99, 99, 99,  99,  99,  99,  99,   28,  29,   99,   99,   99,         99,    99,    56],
 [   99,      99,      99,   "r28", 99,   99,  99,  "r28",   "r28",    99,        99,  99,   99,"r28",  99, 99,   99,   99, "r28",     99,   99,   99, 99, 99,  99,  99,  99,  99,   99,  99,   99,   99,   99,         99,    99,    99],
 [   99,      99,      99,      99, 99,   99,  99,     99,     99,     99,        99,  99,   99,   99,  99, 99,   99,   99,    99,     99,"r10",   99, 99, 99,  99,  99,  99,  99,   99,  99,   99,   99,   99,         99,    99,    99],
-[   99,      99,      99,    "r5", 99,   99,  99,   "r5",   "r5",     99,        99,  99,   99, "r5",  99, 99,   99,   99,    99,     99,   99,   99, 99, 99,  99,  99,  99,  99,   99,  99,   99,   99,   99,         99,    99,    99]
+[   99,      99,      99,    "r5", 99,   99,  99,   "r5",   "r5",     99,        99,  99,   99, "r5",  99, 99,   99,   99,    99,     99,   99,   99, 99, 99,  99,  99,  99,  99,   99,  99,   99,   99,   99,         99,    99,    99]]
 
 gramatica = ["P'->1", "P->3", "V->2", "LV->2","LV->2", "D->3", "TIPO->1", "TIPO->1", "TIPO->1", "A->2", "ES->3", "ES->3", "ARG->1", "ARG->1", "ARG->1", "A->2", "CMD->4", "LD->3", "LD->1", "OPRD->1", "OPRD->1", "A->2", "COND->2", "CABECALHO->5", "EXP_R->3", "CORPO->2", "CORPO->2", "CORPO->2", "CORPO->1", "A->1"]
 coluna = [ "inicio", "varinicio", "varfim", "id", "pt_v", "Inteiro", "real", "leia", "escreva", "literal", "num", "rcb", "opm", "se", "ab_p", "fc_p", "entao", "opr","fimse", "fim", "$", "P", "V", "LV", "D", "TIPO", "A", "ES", "ARG", "CMD", "LD", "OPRD", "COND","CABECALHO","EXP_R", "CORPO"]
 producoes = ["P'->P", "P->inicio V A", "V-> varinicio LV", "LV->D LV","LV-> varfim ;", "D-> id TIPO ;", "TIPO-> int", "TIPO->real", "TIPO-> literal", "A->ES A", "ES-> leia id ;", "ES-> escreva ARG ;", "ARG-> literal", "ARG-> num", "ARG->id", "A-> CMD A", "CMD-> id rcb LD ;", "LD-> OPRD opm OPRD", "LD-> OPRD", "OPRD-> id", "OPRD-> num", "A-> COND A", "COND-> CABECALHO CORPO", "CABECALHO-> se ( EXP_R ) entao", "EXP_R->OPRD opr OPRD", "CORPO-> ES CORPO", "CORPO-> CMD CORPO", "CORPO-> COND CORPO", "CORPO->fimse", "A->fim"]
 
 def separa_reducao(reducao):
-    return reducao.replace("r", "")
+    a = reducao.replace("r", "")
+    return int(a)
 
 def __main__():
     stack = [0]
@@ -81,31 +82,37 @@ def __main__():
         print(a)
         i = 0
         while(i < len(a)):
-            print(a[i])
-            indice = coluna.index(a[i])
-            print("s %d", s)
-            print(indice)
+            try:
+                indice = coluna.index(a[i])
+            except:
+                print("error")
+                break
             auxiliar = tabela_slr[s][indice]
             print(auxiliar)
-            auxiliar = str(auxiliar)
-            if(auxiliar.isdigit() == True and auxiliar != 99):
+            s = auxiliar
+            if(str(auxiliar).isdigit() and auxiliar == 00):
+                print("Aceita!!")
+                break
+            elif(str(auxiliar).isdigit() == True and auxiliar != 99):
                 stack.append(coluna[indice])
                 stack.append(auxiliar)
+                i += 1
                 print(stack)
-                s = tabela_slr[s][indice]
             else:
                 aux2 = gramatica[separa_reducao(auxiliar)-1]
                 aux2 = aux2.split("->")
                 split_esq = aux2[0]
-                split_dir = aux2[1]
+                split_dir = int(aux2[1]) * 2
                 aux3 = 0
-                while(aux3 < split_dir-1):
+                while(aux3 < split_dir):
                     stack.pop()
                     aux3 += 1
                 stack.append(split_esq)
-                print(producoes[separa_reducao(int(auxiliar))-1])
-                s = tabela_slr[coluna.index(stack[len(stack) - 1])][stack[len(stack)-2]]
-            i+= 1
+                print(producoes[separa_reducao(auxiliar)-1])
+                indiceaux = int(coluna.index(stack[len(stack) - 1]))
+                indiceaux2 = int(stack[len(stack) - 2])
+                stack.append(tabela_slr[indiceaux2][indiceaux])
+                s = tabela_slr[indiceaux2][indiceaux]
     fonte.close()
 
 
