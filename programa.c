1f507777bb547ad979f5939b708625ceb524a781
Matheus Vitório <mvfc@users.noreply.github.com>
2017-12-14 00:21:48 -0200

    Add files via upload

---
+++ programa.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+typedef char literal[256];
+void main(void)
+{
+
+literal A;
+int B;
+int D;
+double C;
+
+
+
+printf("Digite B");
+scanf("%d", &B);
+printf("Digite A:");
+scanf("%s", A);
+
+id T0
+T0 = 2 > B
+if(T0) {
+
+id T1
+T1 = 4 <= B
+if(T1) {
+printf("B esta entre 2 e 4");
+}
+}
+int T2
+T2 = B + 1
+B=T2
+int T3
+T3 = B + 2
+B=T3
+int T4
+T4 = B + 3
+B=T4
+D=B
+C=5.0
+printf("\nB=\n");
+printf(D);
+printf("\n");
+printf(C);
+printf("\n");
+printf(A);
+
+}
