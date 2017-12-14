6b9d199ac05375cfbbf72956aaea654eb666b5d1
Matheus Vitório <mvfc@users.noreply.github.com>
2017-12-14 00:19:28 -0200

    Update lexico.py

--- lexico.py
+++ lexico.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
 
+import analisadorsintatico
+
 node = namedtuple("node", "Lexema Token")
 
 c = node("inicio", "inicio")
@@ -18,7 +20,8 @@ o = node("real", "real")
 
 tabelaSimbolo = [c, d, e, f, g, h, i, j, k, l, m, n, o]
 
-retorno = []
+tokens = []
+lexemas = []
 
 def lexico(linha):
     i = 1
@@ -27,6 +30,7 @@ def lexico(linha):
         while(len(linha) < i or linha[i].isalpha() == True or linha[i].isdigit() == True or linha[i] == "_"):
             i = i + 1
         criaNode(linha[:i], "id")
+        analisadorsintatico.__settipo__("id", len(tokens))
         if(linha[i:] != ""):
             if(linha[i] == " "):
                 i += 1
@@ -35,58 +39,77 @@ def lexico(linha):
         while(len(linha) < i or linha[i].isdigit() == True or linha[i] == "E" or linha[i] == "."):
             i = i + 1
         criaNode(linha[:i], "num")
+        analisadorsintatico.__settipo__("num", len(tokens))
         if (linha[i:] != ""):
             if (linha[i] == " "):
                 i += 1
             lexico(linha[i:])
     elif(linha[0] == "("):
         criaNode(linha[:1], "ab_p")
+        analisadorsintatico.__settipo__("(", len(tokens))
         if (linha[1:] != ""):
             lexico(linha[1:])
 
     elif(linha[0] == ")"):
         criaNode(linha[:1], "fc_p")
+        analisadorsintatico.__settipo__(")", len(tokens))
         if (linha[1:] != ""):
             lexico(linha[1:])
 
     elif(linha[0] == "+" or linha[0] == "-" or linha[0] == "/" or linha[0] == "*"):
+        aux = linha[0]
         criaNode(linha[:1], "opm")
+        analisadorsintatico.__settipo__(aux, len(tokens))
         if (linha[1:] != ""):
             lexico(linha[1:])
 
     elif(linha[0] == ";"):
         criaNode(linha[:1], "pt_v")
+        analisadorsintatico.__settipo__(";", len(tokens))
         if (linha[1:] != ""):
             lexico(linha[1:])
 
     elif(linha[0] == "<" and linha[1] == "-"):
         criaNode(linha[:2], "rcb")
+        analisadorsintatico.__settipo__("=", len(tokens))
         if (linha[2:] != ""):
             lexico(linha[2:])
 
     elif(linha[0] == "<" and linha[1] == "="):
+        aux = linha[0]
+        aux = aux + linha[1]
         criaNode(linha[:2], "opr")
+        analisadorsintatico.__settipo__(aux, len(tokens))
         if (linha[2:] != ""):
             lexico(linha[2:])
 
     elif(linha[0] == ">" and linha[1] == "="):
+        aux = linha[0]
+        aux = aux + linha[1]
         criaNode(linha[:2], "opr")
+        analisadorsintatico.__settipo__(aux, len(tokens))
         if (linha[2:] != ""):
             lexico(linha[2:])
 
     elif(linha[0] == "<" and linha[1] == ">"):
+        aux = linha[0]
+        aux = aux + linha[1]
         criaNode(linha[:2], "opr")
+        analisadorsintatico.__settipo__(aux, len(tokens))
         if (linha[2:] != ""):
             lexico(linha[2:])
 
     elif(linha[0] == "<" or linha[0] == ">" or linha[0] == "="):
+        aux = linha[0]
         criaNode(linha[:1], "opr")
+        analisadorsintatico.__settipo__(aux, len(tokens))
         if (linha[1:] != ""):
             lexico(linha[1:])
     elif(linha[0] == '"'):
         while(len(linha) < i or linha[i] != '"'):
             i = i + 1
-        criaNode(linha[:i], "ARG")
+        criaNode(linha[:i], "literal")
+        analisadorsintatico.__settipo__("literal", len(tokens))
         i += 1
         if (linha[i:] != ""):
             if (linha[i] == " "):
@@ -94,18 +117,37 @@ def lexico(linha):
             lexico(linha[i:])
     elif(linha[0] == ""):
         criaNode("", "EOF")
+        analisadorsintatico.__settipo__("EOF", len(tokens))
     elif (linha[0] == "$"):
         criaNode("$", "$")
+        analisadorsintatico.__settipo__("$", len(tokens))
 
 def criaNode(lexema, token):
     new = node(lexema, token)
     newrsv = node(lexema, lexema)
     check = nodeExists(new, newrsv)
     if (check == 2):
-        retorno.append(lexema)
+        tokens.append(lexema)
+        lexemas.append(lexema)
     else:
         tabelaSimbolo.append(new)
-        retorno.append(token)
+        tokens.append(token)
+        lexemas.append(lexema)
+
+
+def __gettoken__(i):
+    return tokens[i]
+
+def __getlexema__(i):
+    return lexemas[i]
+
+def __settoken__(token, i):
+    tokensi[i] = token
+
+def __setlexema__(lexema, i):
+    lexemas[i] = lexema
+def __getindex__(lexema):
+    return lexemas.index(lexema)
 
 def nodeExists(node, nodersv):
     i = 0
@@ -117,31 +159,12 @@ def nodeExists(node, nodersv):
         i = i + 1
     return 0
 
-"""def MostrarTabela():
-    open("output.txt", "w").close()
-    saida = open("output.txt", "w")
-    indexval = 0
-    for i in range(len(tabelaSimbolo)):
-        print(tabelaSimbolo[indexval])
-        saida.write(str(tabelaSimbolo[indexval]).replace("node",""))
-        saida.write("\n")
-        indexval += 1
-    print(retorno)
-
-def __main__():
+def __init__():
     arquivo = open("texto.alg", "r")
     linha = arquivo.readline()
     while(linha != ""):
         lexico(linha)
         linha = arquivo.readline()
     if(linha == ""):
-        criaNode("", "EOF")
-    MostrarTabela()"""
-
-def __main__(args):
-    global retorno
-    retorno = []
-    tabelaSimbolo = []
-    tabelaSimbolo = [c, d, e, f, g, h, i, j, k, l, m, n, o]
-    lexico(args)
-    return retorno
+        criaNode("$", "EOF")
+    arquivo.close()
